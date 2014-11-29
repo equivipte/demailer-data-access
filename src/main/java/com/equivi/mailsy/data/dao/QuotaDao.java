6f03a32d70f7552f3f1aed3689fcef465229fca8
Teguh Wanrasagita <tsagita@hotmail.com>
2014-11-30 01:28:50 +0800

    add annotation

--- src/main/java/com/equivi/mailsy/data/dao/QuotaDao.java
+++ src/main/java/com/equivi/mailsy/data/dao/QuotaDao.java
@@ -4,6 +4,8 @@ package com.equivi.mailsy.data.dao;
 import com.equivi.mailsy.data.entity.QuotaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QuotaDao extends JpaRepository<QuotaEntity, Long>, QueryDslPredicateExecutor<QuotaEntity>  {
 }
